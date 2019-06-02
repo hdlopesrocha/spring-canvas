f92d82098ed6c5baf9518f54efa3ff6b20c3ed9c
Henrique Rocha <hdlopesrocha91@gmail.com>
2019-06-02 02:25:23 +0200

    01 - Draw sine

--- code.js
+++ code.js
@@ -1,7 +1,29 @@
+var canvas;	
+
 $(document).ready(function() {
+	canvas = document.getElementById("myCanvas");
 	window.requestAnimationFrame(loop);
 });
 
 function draw(time) {
-	// TODO
+	var canvasContext = canvas.getContext("2d");
+	canvasContext.clearRect(0, 0, canvas.width, canvas.height);  
+	canvasContext.beginPath();
+	canvasContext.lineWidth = 2;
+	canvasContext.strokeStyle = '#fff';
+
+	var first = true;
+	for (var t=0; t <= 1; t+= 0.01) {
+		var x = t*canvas.width;
+		var centerY = canvas.height/2;
+		var amplitude = canvas.height/2;
+		var y = centerY + amplitude*Math.sin((t+time)*2*Math.PI);
+		if (first) {
+			first = false;
+			canvasContext.moveTo(x, canvas.height-y);
+		} else {
+			canvasContext.lineTo(x, canvas.height-y);
+		}
+	}
+	canvasContext.stroke();
 }
